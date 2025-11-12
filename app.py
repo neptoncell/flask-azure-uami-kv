7a64370191b4dc8eb204e34335319bb091ee312b
canis9 <canislupus577@gmail.com>
2025-11-12 11:54:53 +0100

    Initial Flask + Azure UAMI + KV app

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 from flask import Flask
-from azure.identity import ManagedIdentityCredential
+from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
-import os
 
 app = Flask(__name__)
 
+keyvault_name = "kv-flask-demo"
+secret_name = "MySecret"
+KVUri = f"https://{keyvault_name}.vault.azure.net"
+
+credential = DefaultAzureCredential()
+client = SecretClient(vault_url=KVUri, credential=credential)
+
 @app.route("/")
 def index():
-    key_vault_name = os.getenv("KEY_VAULT_NAME")
-    secret_name = os.getenv("SECRET_NAME")
-    kv_uri = f"https://{key_vault_name}.vault.azure.net/"
-
-    credential = ManagedIdentityCredential()  # utilise UAMI si configurée
-    client = SecretClient(vault_url=kv_uri, credential=credential)
-
-    secret = client.get_secret(secret_name)
-    return f"Le secret est : {secret.value}"
+    try:
+        secret = client.get_secret(secret_name)
+        secret_value = secret.value
+    except Exception as e:
+        secret_value = f"Erreur lors de la lecture du secret : {e}"
+    return f"Le secret est : {secret_value}"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=5002)
